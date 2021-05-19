685d3b1c43140a92ec95e845c54cdb036ce0a032
Jean-Sébastien Goupil <contact@jsgoupil.com>
2021-05-19 15:36:58 -0700

    Initial commit.

---
+++ example/BarcodeAztec.Core.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace BarcodeDataMatrix.Core.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // See each barcode file to see how you can save to a file or a MemoryStream.
+            await ExampleDataMatrix.CreateAsync("barcode_datamatrix.png");
+        }
+    }
+}
